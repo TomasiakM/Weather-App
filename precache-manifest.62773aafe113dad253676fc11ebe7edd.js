a490973215f16a20372e721c86050c1d8ba70185
TomasiakM <marcintomasiak00@gmail.com>
2020-10-13 11:16:32 +0200

    Updates

---
+++ precache-manifest.62773aafe113dad253676fc11ebe7edd.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2507f9c50ddd7b442b165cf0b486335",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "970c836874dc9803cd9f",
+    "url": "/Weather-App/static/css/2.de438a7c.chunk.css"
+  },
+  {
+    "revision": "b0f6edd74d0cf846467c",
+    "url": "/Weather-App/static/css/main.cb420705.chunk.css"
+  },
+  {
+    "revision": "970c836874dc9803cd9f",
+    "url": "/Weather-App/static/js/2.30a9c0fc.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/Weather-App/static/js/2.30a9c0fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0f6edd74d0cf846467c",
+    "url": "/Weather-App/static/js/main.3d5b454e.chunk.js"
+  },
+  {
+    "revision": "5544dd564f6652b0aab4",
+    "url": "/Weather-App/static/js/runtime-main.0f68437e.js"
+  },
+  {
+    "revision": "18e84acbaf40858040f44758d2b35ecf",
+    "url": "/Weather-App/static/media/background.18e84acb.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/Weather-App/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/Weather-App/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/Weather-App/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/Weather-App/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/Weather-App/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/Weather-App/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/Weather-App/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/Weather-App/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/Weather-App/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/Weather-App/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/Weather-App/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/Weather-App/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/Weather-App/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/Weather-App/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/Weather-App/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/Weather-App/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
